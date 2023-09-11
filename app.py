ef7ec1406fe2a26b1b3c10d6ea1b28462ef63439
solanwala <142795290+solanwala@users.noreply.github.com>
2023-09-11 15:42:41 +0530

    v

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 
-
-code = '''def hello():
-    print("Hello, Streamlit!")'''
-st.code(code, language='python')
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
