f686f87e9fcb8ee197b40be5631d61d6034cd7db
solanwala <142795290+solanwala@users.noreply.github.com>
2023-09-11 15:46:25 +0530

    e

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
+st.write("This is some text.")
 
-st.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-    \sum_{k=0}^{n-1} ar^k =
-    a \left(\frac{1-r^{n}}{1-r}\right)
-    ''')
+st.slider("This is a slider", 0, 100, (25, 75))
