7971122da545269f8ffd70e41660f4d2079d9f04
solanwala <142795290+solanwala@users.noreply.github.com>
2023-09-11 16:17:18 +0530

    f

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import time
 
-st.write("This is some text.")
 
-st.slider("This is a slider", 0, 100, (25, 75))
+progress_text = "Operation in progress. Please wait."
+my_bar = st.progress(0, text=progress_text)
+
+for percent_complete in range(100):
+    time.sleep(0.1)
+    my_bar.progress(percent_complete + 1, text=progress_text)
