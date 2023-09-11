f5e968eadbdec1d8eb42f0ecbee4a49ab3df15fd
solanwala <142795290+solanwala@users.noreply.github.com>
2023-09-11 14:00:52 +0530

    kjgkbg

--- app.py
+++ app.py
@@ -2,5 +2,4 @@ import streamlit as st
 
 
 
-st.caption('This is a string that explains something above.')
-st.caption('A caption with _italics_ :blue[colors] and emojis :sunglasses:')
+st.caption('ye delete ho gya.')
