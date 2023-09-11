d947286d27f72b35c69329f0c9aba79853781ea7
solanwala <142795290+solanwala@users.noreply.github.com>
2023-09-11 16:24:11 +0530

    b

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import time
 
-with st.spinner('Wait for it...'):
-    time.sleep(10)
-st.success('Done!')
+e = RuntimeError('This is an exception of type RuntimeError')
+st.exception(e)
+
