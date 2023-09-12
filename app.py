534ea6f6272af6efc63b89c8090bff72df54624d
solanwala <142795290+solanwala@users.noreply.github.com>
2023-09-12 12:21:48 +0530

    e

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-conn = st.experimental_connection("sql")
-df = conn.query("select * from pet_owners")
-st.dataframe(df)
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.markdown('''
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors].''')
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+multi = '''If you end a line with two spaces,
+a soft return is used for the next line.
+
+Two (or more) newline characters in a row will result in a hard return.
+'''
+st.markdown(multi)
 
