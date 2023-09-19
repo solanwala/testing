c93d4cd552b5fc0c90fe7650e14637030ec9260a
solanwala <142795290+solanwala@users.noreply.github.com>
2023-09-19 16:03:52 +0530

    sfg

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ import pandas as pd
 import numpy as np
 
 ff = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+    {
+        "sales": [
+            [0, 4, 26, 80, 100, 40],
+            [80, 20, 80, 35, 40, 100],
+            [10, 20, 80, 80, 70, 0],
+            [10, 100, 20, 100, 30, 100],
+        ],
+    })
 
-ff = st.data_editor(
+st.data_editor(
     ff,
     column_config={
     "Chart": st.column_config.LineChartColumn("Chart", width="medium",
